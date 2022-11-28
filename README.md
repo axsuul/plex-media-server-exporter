05e36e2f10f7c45227a6ccc186ead3eb78258830
James Hu <hello@james.hu>
2022-11-28 09:42:57 -0800

    Large refactor (#2)

--- README.md
+++ README.md
@@ -17,9 +17,12 @@ These environment variables can be passed into the container (defaults are in pa
   - Plex Media Server address
 * `PLEX_TOKEN`
   - Plex Media Server token
+* `PLEX_TIMEOUT` (`10`)
+  - How long to wait for Plex Media Server to respond
 * `METRICS_PREFIX` (`plex`)
+  - What to prefix metric names with
 * `METRICS_MEDIA_COLLECTING_INTERVAL_SECONDS` (`300`)
-  - How often to throttle collection of media metrics
+  - How often to throttle collection of media metrics which can take longer to complete depending on how large of a library you have
 
 ## Metrics
 
@@ -27,8 +30,8 @@ Served by default via `http://localhost:9594/metrics`
 
 ```
 # TYPE plex_info gauge
-# HELP plex_info Basic server info
-plex_info{version="1.29.2.6364-6d72b0cf6",platform="Linux",state="up"} 1.0
+# HELP plex_info Server diagnostics and hearbeat (1=up, 0=down)
+plex_info{version="1.29.2.6364-6d72b0cf6",platform="Linux"} 1.0
 # TYPE plex_media_count gauge
 # HELP plex_media_count Number of media in library
 plex_media_count{title="Movies",type="movie"} 19318.0
@@ -37,14 +40,17 @@ plex_media_count{title="Audiobooks",type="artist"} 17.0
 plex_media_count{title="Music",type="artist"} 891.0
 # TYPE plex_sessions_count gauge
 # HELP plex_sessions_count Number of current sessions
-plex_sessions_count{state="playing"} 5.0
-plex_sessions_count{state="buffering"} 1.0
-plex_sessions_count{state="paused"} 2.0
+plex_sessions_count{state="buffering"} 0.0
+plex_sessions_count{state="paused"} 1.0
+plex_sessions_count{state="playing"} 2.0
 # TYPE plex_audio_transcode_sessions_count gauge
 # HELP plex_audio_transcode_sessions_count Number of current sessions that are transcoding audio
-plex_audio_transcode_sessions_count{state="playing"} 1.0
-plex_audio_transcode_sessions_count{state="paused"} 1.0
+plex_audio_transcode_sessions_count{state="buffering"} 0.0
+plex_audio_transcode_sessions_count{state="paused"} 0.0
+plex_audio_transcode_sessions_count{state="playing"} 0.0
 # TYPE plex_video_transcode_sessions_count gauge
 # HELP plex_video_transcode_sessions_count Number of current sessions that are transcoding video
+plex_video_transcode_sessions_count{state="buffering"} 0.0
+plex_video_transcode_sessions_count{state="paused"} 0.0
 plex_video_transcode_sessions_count{state="playing"} 1.0
 ```
--- lib/middleware/collector.rb
+++ lib/middleware/collector.rb
@@ -3,26 +3,34 @@ require "http"
 module PlexMediaServerExporter
   module Middleware
     class Collector
+      SESSION_COUNT_METRIC_KINDS = [:all, :audio_transcode, :video_transcode].freeze
+      SESSION_STATES = ["buffering", "paused", "playing"].freeze
+
       def initialize(app)
         @app = app
         @registry = ::Prometheus::Client.registry
+
+        # Plex configs
+        @plex_addr = ENV["PLEX_ADDR"] || "http://localhost:32400"
+        @plex_timeout = ENV["PLEX_TIMEOUT"]&.to_i || 10
+
+        # Metrics configs
         @metrics_prefix = ENV["METRICS_PREFIX"] || "plex"
         @metrics_media_collecting_interval_seconds = ENV["METRICS_MEDIA_COLLECTING_INTERVAL_SECONDS"]&.to_i || 300
-        @sessions_count_metrics = Hash.new { |h, k| h[k] = Hash.new { |hh, kk| hh[kk] = 0 } }
 
         # Initialize metrics
         @metrics = {}
         @metrics[:info] = @registry.gauge(
           :"#{@metrics_prefix}_info",
-          docstring: "Basic server info",
-          labels: [:platform, :state, :version],
+          docstring: "Server diagnostics and heartbeat (1=up, 0=down)",
+          labels: [:platform, :version],
         )
         @metrics[:media_count] = @registry.gauge(
           :"#{@metrics_prefix}_media_count",
           docstring: "Number of media in library",
           labels: [:title, :type],
         )
-        @metrics[:sessions_count] = @registry.gauge(
+        @metrics[:all_sessions_count] = @registry.gauge(
           :"#{@metrics_prefix}_sessions_count",
           docstring: "Number of current sessions",
           labels: [:state],
@@ -50,53 +58,17 @@ module PlexMediaServerExporter
           info_labels[:version] = capabilities_resource.dig("version")
           info_labels[:platform] = capabilities_resource.dig("platform")
 
+          # Value of 1 means there's a heartbeat
           @metrics[:info].set(1,
-            labels: info_labels.merge(state: "up"),
+            labels: info_labels,
           )
 
-          # Reset all session count metrics back to 0 since previous attributes may have changed
-          @sessions_count_metrics.each do |_, kind_metrics|
-            kind_metrics.each do |key, _|
-              kind_metrics[key] = 0
-            end
-          end
-
-          send_plex_api_request(method: :get, endpoint: "/status/sessions")
-            .dig("MediaContainer", "Metadata")
-            .each do |session_resource|
-              state = session_resource.dig("Player", "state")
-
-              if (transcode_session = session_resource.dig("TranscodeSession"))
-                if transcode_session.dig("audioDecision") == "transcode"
-                  @sessions_count_metrics[:audio_transcode][state] += 1
-                  # @audio_transcode_sessions_count_by_state[state] += 1
-                end
-
-                if transcode_session.dig("videoDecision") == "transcode"
-                  @sessions_count_metrics[:video_transcode][state] += 1
-                  # @video_transcode_sessions_count_by_state[state] += 1
-                end
-              end
-
-              @sessions_count_metrics[:default][state] += 1
-            end
-
-          @sessions_count_metrics[:default].each do |state, count|
-            @metrics[:sessions_count].set(count, labels: { state: state })
-          end
-          @sessions_count_metrics[:audio_transcode].each do |state, count|
-            @metrics[:audio_transcode_sessions_count].set(count, labels: { state: state })
-          end
-          @sessions_count_metrics[:video_transcode].each do |state, count|
-            @metrics[:video_transcode_sessions_count].set(count, labels: { state: state })
-          end
-
+          collect_session_metrics
           collect_media_metrics
-
-        # Could not reach Plex so it's down
-        rescue HTTP::ConnectionError
-          @metrics[:info].set(1,
-            labels: info_labels.merge(state: "down"),
+        rescue HTTP::Error
+          # Value of 0 means there's no heartbeat
+          @metrics[:info].set(0,
+            labels: info_labels,
           )
         end
 
@@ -104,6 +76,41 @@ module PlexMediaServerExporter
       end
 
       private
+
+      def collect_session_metrics
+        count_metrics = Hash.new { |h, k| h[k] = {} }
+
+        # Initialize
+        SESSION_COUNT_METRIC_KINDS.each do |metric_kind|
+          SESSION_STATES.each do |state|
+            count_metrics[metric_kind][state] = 0
+          end
+        end
+
+        send_plex_api_request(method: :get, endpoint: "/status/sessions")
+          .dig("MediaContainer", "Metadata")
+          &.each do |session_resource|
+            state = session_resource.dig("Player", "state")
+
+            if (transcode_session = session_resource.dig("TranscodeSession"))
+              if transcode_session.dig("audioDecision") == "transcode"
+                count_metrics[:audio_transcode][state] += 1
+              end
+
+              if transcode_session.dig("videoDecision") == "transcode"
+                count_metrics[:video_transcode][state] += 1
+              end
+            end
+
+            count_metrics[:all][state] += 1
+          end
+
+        SESSION_COUNT_METRIC_KINDS.each do |metric_kind|
+          count_metrics[metric_kind].each do |state, count|
+            @metrics[:"#{metric_kind}_sessions_count"].set(count, labels: { state: state })
+          end
+        end
+      end
 
       def collect_media_metrics
         # Add ability to throttle this in case it negatively impacts
@@ -139,13 +146,13 @@ module PlexMediaServerExporter
       end
 
       def send_plex_api_request(method:, endpoint:, **options)
-        addr = ENV["PLEX_ADDR"] || "http://localhost:32400"
         response = HTTP
+          .timeout(@plex_timeout)
           .headers(
             "X-Plex-Token" => ENV["PLEX_TOKEN"],
             "Accept" => "application/json",
           )
-          .public_send(method, "#{addr}#{endpoint}", **options)
+          .public_send(method, "#{@plex_addr}#{endpoint}", **options)
 
         JSON.parse(response)
       end
